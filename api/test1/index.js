a9df51b5717c5dc1f0e2d4961cb8f03a4344b8c4
Ruedi Schwarzenbach <ruedi.schwarzenbach@gmx.ch>
2021-07-26 15:57:09 +0200

    test1 for trading economics

---
+++ api/test1/index.js
@@ -0,0 +1,27 @@
+const te = require('tradingeconomics');
+
+te.login();
+// data1 = [];
+// data = te.getMarketSnap(symbol = 'aapl:us').then(function(data){
+//     console.log(data);
+//     data1 = data;
+// });
+
+//data2 = await te.getMarketSnap(symbol = 'aapl:us');
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    //const responseMessage = await te.getMarketSnap(symbol = 'aapl:us');
+    const responseMessage = await te.getMarketSnap(symbol = 'ndx:ind');
+     
+    /*
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Sali " + name + "!\nThis HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    */
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
